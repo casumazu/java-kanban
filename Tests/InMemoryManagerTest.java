c5f86b007879406bcf2225289aaceeee6a240d04
casumazu <sergeev.bog@yandex.ru>
2023-04-03 23:54:34 +0400

    ТЗ 7 Редактирование checkIntersections в InMemoryTaskManager

--- Tests/InMemoryManagerTest.java
+++ Tests/InMemoryManagerTest.java
@@ -1,19 +1,5 @@
 import manager.InMemoryTaskManager;
-import manager.Managers;
-import manager.TaskManager;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import tasks.Epic;
-import tasks.Subtask;
-import tasks.Task;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
     @BeforeEach
